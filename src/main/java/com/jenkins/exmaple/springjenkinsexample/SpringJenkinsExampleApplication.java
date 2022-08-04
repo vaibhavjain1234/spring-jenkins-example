9b7324cce10095699359b3aa5ed67c2f682042ef
vjain5 <vjain5@lenovo.com>
2022-08-04 18:15:54 +0530

    API

--- src/main/java/com/jenkins/exmaple/springjenkinsexample/SpringJenkinsExampleApplication.java
+++ src/main/java/com/jenkins/exmaple/springjenkinsexample/SpringJenkinsExampleApplication.java
@@ -6,13 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringJenkinsExampleApplication {
+public class SpringJenkinsExampleApplication extends SpringBootServletInitializer{
 
+	
 	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsExampleApplication.class);
 	
 	@PostConstruct
@@ -21,6 +24,11 @@ public class SpringJenkinsExampleApplication {
 		logger.info("Application started...");
 	}
 	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringJenkinsExampleApplication.class);
+	}
 	
 	public static void main(String[] args) {
 		logger.info("Application Execueted..");
