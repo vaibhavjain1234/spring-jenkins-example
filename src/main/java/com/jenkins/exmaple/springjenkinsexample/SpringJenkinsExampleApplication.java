b58020925962d64c6054e1eb8e063361bf586b4e
vjain5 <vjain5@lenovo.com>
2022-08-04 12:47:46 +0530

    Change in Main class

--- src/main/java/com/jenkins/exmaple/springjenkinsexample/SpringJenkinsExampleApplication.java
+++ src/main/java/com/jenkins/exmaple/springjenkinsexample/SpringJenkinsExampleApplication.java
@@ -6,8 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsExampleApplication {
 
 	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsExampleApplication.class);
@@ -24,4 +27,10 @@ public class SpringJenkinsExampleApplication {
 		SpringApplication.run(SpringJenkinsExampleApplication.class, args);
 	}
 
+	@GetMapping("/testing")
+	public String test()
+	{
+		return "This is spring boot application....";
+	}
+	
 }
