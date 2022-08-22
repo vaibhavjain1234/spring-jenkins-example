ff3723419f59ad15f6a80310e36791776453c98e
vjain5 <vjain5@lenovo.com>
2022-08-22 14:45:26 +0530

    New API

--- src/main/java/com/jenkins/exmaple/springjenkinsexample/SpringJenkinsExampleApplication.java
+++ src/main/java/com/jenkins/exmaple/springjenkinsexample/SpringJenkinsExampleApplication.java
@@ -45,9 +45,15 @@ public class SpringJenkinsExampleApplication extends SpringBootServletInitialize
 	@GetMapping("/one")
 	public String test1()
 	{
-		return "Welcome to the Java World...."
+		return "Welcome to the Java World....";
 	}
 	
 
+	@GetMapping("/two")
+	public String test2()
+	{
+		return "Welcome to the DXC....";
+	}
+	
 	
 }
